b2a1b71a319e2b25b218038728acb4f897775ea0
anyangmaxin@163.com <anyangmaxin@163.com>
2021-07-07 10:36:25 +0800

    增加Movie实体

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+	public class Movie
+	{
+		public int ID { get; set; }
+
+		public string Title { get; set; }
+
+		/// <summary>
+		/// [DataType(DataType.Date)]：[DataType] 属性指定数据的类型 (Date)。 通过此特性
+		/// 用户无需在日期字段中输入时间信息
+		/// 仅显示日期，而非时间信息
+		/// </summary>
+		[DataType(DataType.Date)]
+		public DateTime ReleaseDate { get; set; }
+
+		public string Genre { get; set; }
+
+		public decimal Price { get; set; }
+
+	}
+}
