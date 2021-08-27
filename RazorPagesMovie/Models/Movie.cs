52db83d73ac3883f60836f6a1e9fe8ad84bb86de
anyangmaxin@163.com <anyangmaxin@163.com>
2021-08-27 17:38:52 +0800

    增加字段Rating ,手工更新数据库字段，

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -10,6 +11,9 @@ namespace RazorPagesMovie.Models
 	{
 		public int ID { get; set; }
 
+		/// <summary>
+		/// 名称
+		/// </summary>
 		public string Title { get; set; }
 
 		/// <summary>
@@ -17,12 +21,25 @@ namespace RazorPagesMovie.Models
 		/// 用户无需在日期字段中输入时间信息
 		/// 仅显示日期，而非时间信息
 		/// </summary>
+		[Display(Name = "Release Date")]
 		[DataType(DataType.Date)]
 		public DateTime ReleaseDate { get; set; }
 
+		/// <summary>
+		/// 流派
+		/// </summary>
 		public string Genre { get; set; }
 
+		/// <summary>
+		/// 价格
+		/// </summary>
+		[Column(TypeName = "decimal(18,2)")]
 		public decimal Price { get; set; }
+
+		/// <summary>
+		/// 分级
+		/// </summary>
+		public string Rating { get; set; }
 
 	}
 }
